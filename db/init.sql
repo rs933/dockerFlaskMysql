2e16e8415221f77f3b5cec902860667733b30244
rs933 <rs933@njit.edu>
2021-04-14 11:07:24 -0400

    added html templates to edit, add new data, and delete data from database

--- db/init.sql
+++ db/init.sql
@@ -1,23 +1,21 @@
---  creating database with raw data
-
 CREATE DATABASE citiesData;
 use citiesData;
 
 CREATE TABLE IF NOT EXISTS tblCitiesImport (
+    `id` int AUTO_INCREMENT,
     `fldName` VARCHAR(21) CHARACTER SET utf8,
     `fldLat` NUMERIC(6, 4),
     `fldLong` NUMERIC(7, 4),
     `fldCountry` VARCHAR(19) CHARACTER SET utf8,
-    `fldAbreviation` VARCHAR(3) CHARACTER SET utf8,
+    `fldAbbreviation` VARCHAR(3) CHARACTER SET utf8,
     `fldCapitalStatus` VARCHAR(7) CHARACTER SET utf8,
-    `fldPopulation` INT
+    `fldPopulation` INT,
+    PRIMARY KEY (`id`)
 );
-INSERT INTO tblCitiesImport VALUES
+INSERT INTO tblCitiesImport (fldName,fldLat,fldLong,fldCountry,fldAbbreviation,fldCapitalStatus,fldPopulation) VALUES
     ('Tokyo',35.685,139.7514,'Japan','JPN','primary',35676000),
-    ('New York',40.6943,-73.9249,'United States','USA','NA',19354922),
     ('Mexico City',19.4424,-99.131,'Mexico','MEX','primary',19028000),
     ('Los Angeles',34.1139,-118.4068,'United States','USA','NA',12815475),
-    ('Dhaka',23.7231,90.4086,'Bangladesh','BGD','primary',12797394),
     ('Buenos Aires',-34.6025,-58.3975,'Argentina','ARG','primary',12795000),
     ('Cairo',30.05,31.25,'Egypt','EGY','primary',11893000),
     ('Beijing',39.9289,116.3883,'China','CHN','primary',11106000),
